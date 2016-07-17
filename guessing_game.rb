772f3df7951624c35f3849376c7f17c77b9a2499
Ava Evans <avaevansm@gmail.com>
2016-07-17 18:10:08 -0400

    working game

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,38 +1,30 @@
-counter = 0
+counter = 1
 generated_number = rand(1..100)
-user_input = []
-puts generated_number
+user_inputs = []
+# puts generated_number
 
-if [].include? (user_input.last) == true
-   puts "Are you ok?"
- end
-
-until counter == 5
+until counter == 6
   puts counter
   counter = counter +1
 
-   puts "Guess a number between 1 and 100"
-   user_input.push(gets.chomp.to_i)
+  puts "Guess a number between 1 and 100"
+  current_guess = gets.chomp.to_i
 
-
-
-if  counter == 5
+  # p user_inputs
+  if  counter == 6
     p "You lose"
-    elsif user_input.last > generated_number
+  elsif user_inputs.include?(current_guess) == true
+    p "Are you ok?"
+    user_inputs.push(current_guess)
+  elsif current_guess > generated_number
     p "That's too high. Guess again!"
-    elsif user_input.last < generated_number
+    user_inputs.push(current_guess)
+  elsif current_guess < generated_number
     p "That's too low. Guess again!"
-    elsif user_input.last == generated_number
-      counter = 5
+    user_inputs.push(current_guess)
+  else current_guess == generated_number
+    counter = 6
     p "You win!"
   end
 
 end
-
-=begin
-  Line 3 is supposed to represent the numbers that the user
-will input. I want Ruby to detect if a number from that list
-has already been entered. i.e. react if a duplicate guess is entered.
-  On lines 22 & 23 I want Ruby to detect if the attempts exede 5, and
-if that is true, for the program to say "you lose" then end.
-=end
---
+++ guessing_game_test.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require "./guessing_game"
+
+class GuessingGameTest < Minitest::Test
+
+def test_pick_random_number
+
+  generated_number = rand(1..100)
+  puts generated_number
+
+  assert_equal
+
+end
+
+
+
+
+end
